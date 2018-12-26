65ff9f949312607ec0168ccae51df2a69074df24
Nan Zhang <zhangnan@fudan.edu.cn>
2018-12-26 17:29:34 +0800

    t

--- teaching/Fall2018/SCRch9_1.R
+++ teaching/Fall2018/SCRch9_1.R
@@ -172,54 +172,6 @@ hist(xt[101:m], main="", xlab="p", prob=TRUE)
 print(mean(xt[101:m]))
 
 
-### Example 9.7 (Gibbs sampler: Bivariate distribution)
-
-#initialize constants and parameters
-N <- 5000               #length of chain
-burn <- 1000            #burn-in length
-X <- matrix(0, N, 2)    #the chain, a bivariate sample
-
-rho <- -.75             #correlation
-mu1 <- 0
-mu2 <- 2
-sigma1 <- 1
-sigma2 <- .5
-s1 <- sqrt(1-rho^2)*sigma1
-s2 <- sqrt(1-rho^2)*sigma2
-
-###### generate the chain #####
-
-X[1, ] <- c(mu1, mu2)            #initialize
-
-for (i in 2:N) {
-  x2 <- X[i-1, 2]
-  m1 <- mu1 + rho * (x2 - mu2) * sigma1/sigma2
-  X[i, 1] <- rnorm(1, m1, s1)
-  x1 <- X[i, 1]
-  m2 <- mu2 + rho * (x1 - mu1) * sigma2/sigma1
-  X[i, 2] <- rnorm(1, m2, s2)
-}
-
-b <- burn + 1
-x <- X[b:N, ]
-
-# compare sample statistics to parameters
-colMeans(x)
-cov(x)
-cor(x)
-
-library(MASS)
-S = matrix(c(sigma1^2, rho*sigma1*sigma2,
-             rho*sigma1*sigma2, sigma2^2),2)
-x_MASS = mvrnorm(n=dim(x)[1],mu=c(mu1, mu2), Sigma=S)
-
-par(mfrow=c(1,2))
-plot(x, cex=.5, xlab=bquote(X[1]),
-     ylab=bquote(X[2]), ylim=range(x[,2]), main='MCMC')
-plot(x_MASS, cex=.5, xlab=bquote(X[1]),
-     ylab=bquote(X[2]), ylim=range(x[,2]), main='MASS')
-
-
 
 
 
