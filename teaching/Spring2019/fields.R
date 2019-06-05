a7829eb04446cb1e11ebec0544a267b0edc9dd2c
Nan Zhang <zhangnan@fudan.edu.cn>
2019-06-05 13:17:57 +0800

    t

---
+++ teaching/Spring2019/fields.R
@@ -0,0 +1,69 @@
+library(fields)
+
+### ozone data set, day 16
+## Quilt plot
+data(ozone2)
+# plot 16 day of ozone data set
+quilt.plot(ozone2$lon.lat, ozone2$y[16,])
+US(add=TRUE, col="grey", lwd=2)
+
+# compute variogram for the midwest ozone field 
+
+good<- !is.na(ozone2$y[16,])
+x<- ozone2$lon.lat[good,] 
+y<- ozone2$y[16,good]
+
+## variogram plots
+par(mfrow=c(1,2))
+look<-vgram(x,y, N=15, lon.lat=TRUE)
+plot(look, pch=19)
+
+## or some boxplot bin summaries
+brk<- seq(0, 250,, (25 + 1) ) # will give 25 bins.
+boxplotVGram(look, breaks=brk, plot.args=list(type="o"))
+plot(look, add=TRUE, breaks=brk, col=4)
+dev.off()
+
+
+#############################
+# Kriging
+# a 2-d example 
+# fitting a surface to ozone  
+# measurements. Exponential covariance, range parameter is 20 (in miles) 
+
+fit <- Krig(ChicagoO3$x, ChicagoO3$y, theta=20)  
+
+summary(fit) # summary of fit 
+par(mfrow=c(2,2))
+plot(fit) # four diagnostic plots of fit  
+dev.off()
+
+surface(fit, type="C") # look at the surface 
+
+# predict at data
+predict(fit)
+
+# predict using 7.5 effective degrees of freedom:
+predict(fit, df=7.5)
+
+# predict on a grid (grid chosen here by defaults)
+out<- predictSurface(fit)
+surface(out, type="C") # option "C" our favorite
+
+# predict at arbitrary points (10,-10) and (20, 15)
+xnew<- rbind(c(10, -10), c(20, 15))
+predict(fit, xnew)
+
+# standard errors of prediction based on covariance model.  
+predictSE(fit, xnew)
+
+# surface of standard errors on a default grid
+predictSurfaceSE(fit)-> out.p # this takes some time!
+surface(out.p, type="C")
+points(fit$x)
+
+## Using another stationary covariance. 
+# smoothness is the shape parameter for the Matern
+fit <- Krig(ChicagoO3$x, ChicagoO3$y, 
+            Covariance="Matern", theta=10, smoothness=1.0)  
+summary( fit)
---
+++ teaching/Spring2019/variogram.R
@@ -0,0 +1,56 @@
+library(fields)
+data=read.table('data1.txt',header=T)
+nlon=length(unique(data[,1]))
+nlat=length(unique(data[,2]))
+loc=data[,1:2]
+y=data[,3]
+varigram=vgram(loc, y, lon.lat=TRUE,N=15)
+
+par(mfrow=c(1,2))
+##here obtained is semivarigram,then times 2 to get varigram##
+##Plot the varigram cloud 
+plot(varigram$d,2*varigram$vgram,xlab="distance",ylab="varigram",main="Variogram cloud")
+lines(varigram$centers, varigram$stats["mean",], col=4,lwd=3)
+
+
+bplot.xy(varigram$d, sqrt(2*varigram$vgram),ylab="sqrt(VG)", N=15, outlier=TRUE, main="Boxplots of variogram")
+lines(varigram$centers, sqrt(2*varigram$stats["mean",]), col=4,lwd=3)
+
+graphics.off()
+
+
+###directional variogram, fix the latitude.
+data1=data[101:150,]
+data2=data[201:250,]
+data3=data[301:350,]
+data4=data[401:450,]
+
+vgram_lat1=vgram(data1[,1:2], data1[,3], lon.lat=TRUE, N=10)
+vgram_lat2=vgram(data2[,1:2], data2[,3], lon.lat=TRUE, N=10)
+vgram_lat3=vgram(data3[,1:2], data3[,3], lon.lat=TRUE, N=10)
+vgram_lat4=vgram(data4[,1:2], data4[,3], lon.lat=TRUE, N=10)
+
+pdf('lat.pdf',width=14,height=14)
+par(mfrow=c(2,2))
+bplot.xy(vgram_lat1$d, sqrt(2*vgram_lat1$vgram),ylab="sqrt(VG)", N=10, outlier=TRUE, main="latitude -49.72")
+lines(vgram_lat1$centers, sqrt(2*vgram_lat1$stats["mean",]), col=4, lwd=3)
+
+bplot.xy(vgram_lat2$d, sqrt(2*vgram_lat2$vgram),ylab="sqrt(VG)", N=10, outlier=TRUE, main="latitude -9.94")
+lines(vgram_lat2$centers, sqrt(2*vgram_lat2$stats["mean",]), col=4, lwd=3)
+
+bplot.xy(vgram_lat3$d, sqrt(2*vgram_lat3$vgram),ylab="sqrt(VG)", N=10, outlier=TRUE, main="latitude 29.83")
+lines(vgram_lat3$centers, sqrt(2*vgram_lat3$stats["mean",]), col=4, lwd=3)
+
+bplot.xy(vgram_lat4$d, sqrt(2*vgram_lat4$vgram),ylab="sqrt(VG)", N=10, outlier=TRUE, main="latitude 69.61")
+lines(vgram_lat4$centers, sqrt(2*vgram_lat4$stats["mean",]), col=4, lwd=3)
+
+graphics.off()
+
+# 
+# ###use variog4 to plot variogram in 4 directions
+# library(geoR)
+# data2=as.list(data)
+# var4=variog4(coords =loc,data =y)
+# 
+# plot(var4,lwd=3)
+# graphics.off()
